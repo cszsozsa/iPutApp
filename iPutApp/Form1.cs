f18b8dfbb577f02785bed11c7ff4c6913b65544a
Császár Zsolt <csaszarzsolti.92@gmail.com>
2020-02-21 16:47:47 +0100

    Initial commit

---
+++ iPutApp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using InputForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iPutApp
+{
+    public partial class Form1 : Form
+    {
+        private InputForm inp;
+        public Form1()
+        {
+            InitializeComponent();
+
+            inp = new InputForm(10, 10, this);
+
+            inp.AddField("name", "Teljes név");
+            inp.AddField("email", "E-Mail cím");
+            inp.AddField("born", "Születési dátum");
+            inp.AddButton("Regisztráció", ButtonClick);
+        }
+        
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(inp.GetValue("name"));
+        }
+    }
+}
